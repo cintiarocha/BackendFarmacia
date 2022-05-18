5e3afcd7db29002ba970b44ebed8ed36556a0b23
Cintia Rocha <cintia.rocha95@gmail.com>
2022-05-18 15:21:34 -0300

    Atualização do Repository

--- farmacia/src/main/java/com/farmacia/farmacia/repository/CategoriaRepository.java
+++ farmacia/src/main/java/com/farmacia/farmacia/repository/CategoriaRepository.java
@@ -4,12 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.farmacia.farmacia.model.CategoriaModel;
 
-
-
-
+@Repository
 public interface CategoriaRepository extends JpaRepository<CategoriaModel, Long> {
 	public List<CategoriaModel> findAllByNomeContainingIgnoreCase (@Param("nome")String nome);
 	
